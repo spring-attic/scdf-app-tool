2c3ced0972dc04afc129b3d2e84f4ff489f5e364
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-11 22:13:28 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * http://www.apache.org/licenses/LICENSE-2.0 migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).
    * http://bit.ly/ migrated to:
      https://bit.ly/ ([https](https://bit.ly/) result 302).
    
    # Ignored
    These URLs were intentionally ignored.
    
    * http://maven.apache.org/POM/4.0.0
    * http://maven.apache.org/xsd/maven-4.0.0.xsd
    * http://www.w3.org/2001/XMLSchema-instance

--- config/update-properties.sh
+++ config/update-properties.sh
@@ -3,7 +3,7 @@
 STREAM_APP_VERSION=Celsius-SR2
 TASK_APP_VERSION=Clark-GA
 
-wget -q -O rabbit-stream-apps.properties "http://bit.ly/$STREAM_APP_VERSION-stream-applications-rabbit-maven"
-wget -q -O kafka-10-stream-apps.properties "http://bit.ly/$STREAM_APP_VERSION-stream-applications-kafka-10-maven"
-wget -q -O task-apps.properties  "http://bit.ly/$TASK_APP_VERSION-task-applications-maven"
+wget -q -O rabbit-stream-apps.properties "https://bit.ly/$STREAM_APP_VERSION-stream-applications-rabbit-maven"
+wget -q -O kafka-10-stream-apps.properties "https://bit.ly/$STREAM_APP_VERSION-stream-applications-kafka-10-maven"
+wget -q -O task-apps.properties  "https://bit.ly/$TASK_APP_VERSION-task-applications-maven"
 
